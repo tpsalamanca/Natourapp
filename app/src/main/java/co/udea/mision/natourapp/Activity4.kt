3c1374eb80c608dd838d477882d5439d9415d296
Juan Sebastián Bustamante <jusebu@hotmail.com>
2021-11-14 09:50:36 -0500

    Adición de Activity4 al proyecto

---
+++ app/src/main/java/co/udea/mision/natourapp/Activity4.kt
@@ -0,0 +1,11 @@
+package co.udea.mision.natourapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity4)
+    }
+}
